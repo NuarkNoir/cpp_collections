5d8455838b5ed633100bebc04b0ffd26fb99fb4d
Andrew <mrcluster@ya.ru>
2020-03-09 12:35:28 +0700

    Rewritten container class

--- container.h
+++ container.h
@@ -8,20 +8,44 @@ class container {
 static_assert(std::is_pod<T>::value, "T must be POD");
 
 private:
-    int _size = 0;
-    int _capacity = 20;
+    std::size_t _size = 0;
+    std::size_t _capacity = 20;
     T* elements = nullptr;
 
+    void _reallocate_mem() {
+        T* tmp = new T[this->_capacity];
+        if (this->elements != nullptr) {
+            for (std::size_t i = 0; i < this->_size; i++) {
+                tmp[i] = T(this->elements[i]);
+            }
+        }
+        delete[] this->elements;
+        this->elements = tmp;
+    }
+
 public:
+    container() {
+        this->_reallocate_mem();
+    }
+    container(std::size_t initial_capacity) :_capacity(initial_capacity) {
+        this->_reallocate_mem();
+    }
+    container(std::size_t initial_capacity, T default_element) :_capacity(initial_capacity) {
+        this->assign(initial_capacity, default_element);
+    }
     virtual ~container() {
         delete[] this->elements;
     }
+    
+    void init() {
+        _reallocate_mem();
+    }
 
-    int size() {
+    std::size_t size() {
         return this->_size;
     }
 
-    int capacity() {
+    std::size_t capacity() {
         return this->_capacity;
     }
 
